7a04ee8e974ec1e2b14dc4459431abe46de51a2c
Harshit Shah <harshitspeaks@gmail.com>
2017-07-11 08:42:30 -0400

    Add css for chat page

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ io.on('connection', (socket) => {
         console.log('createMessage', newMessage);
 
         io.emit('newMessage', generateMessage(newMessage.from, newMessage.text));
-        callback('This is from server');
+        callback();
     });
 
     socket.on('createLocationMessage', (coords) => {
